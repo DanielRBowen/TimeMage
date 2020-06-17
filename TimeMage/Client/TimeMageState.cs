942ee51a60e963dc22c3352e6bac8aa88b108df2
DanielRBowen <danielrbowen@live.com>
2020-06-17 14:24:28 -0600

    Save the timers and intervalsets to local storage after each edit to them. Load them up OnInitializedAsync.

--- TimeMage/Client/TimeMageState.cs
+++ TimeMage/Client/TimeMageState.cs
@@ -1,12 +1,44 @@
 ﻿using System.Collections.Generic;
 using TimeMage.Shared;
+using Blazored.LocalStorage;
+using System.Threading.Tasks;
 
 namespace TimeMage.Client
 {
     public class TimeMageState
     {
+        private readonly ILocalStorageService _localStorageService;
+
+        public TimeMageState(ILocalStorageService localStorageService)
+        {
+            _localStorageService = localStorageService;
+        }
+
         public List<TmTimer> Timers { get; set; } = new List<TmTimer>();
 
         public List<IntervalSet> IntervalSets { get; set; } = new List<IntervalSet>();
+
+        public async Task SaveAsync()
+        {
+            await _localStorageService.SetItemAsync<List<TmTimer>>(nameof(Timers), Timers);
+            await _localStorageService.SetItemAsync(nameof(IntervalSets), IntervalSets);
+        }
+
+        public async Task LoadAsync()
+        {
+            var timers = await _localStorageService.GetItemAsync<List<TmTimer>>(nameof(Timers));
+
+            if (timers != null)
+            {
+                Timers = timers;
+            }
+
+            var intervalSets = await _localStorageService.GetItemAsync<List<IntervalSet>>(nameof(IntervalSets));
+
+            if (intervalSets != null)
+            {
+                IntervalSets = intervalSets;
+            }
+        }
     }
 }
